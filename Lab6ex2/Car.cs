2806689e5bc8b47afd571acd712704e6c832af84
Furdui Robert <Furdui Robert@DESKTOP-E78TGOH>
2023-07-26 03:17:14 -0700

    Add project files.

---
+++ Lab6ex2/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6ex2
+{
+    internal class Car
+    {
+        public string marca;
+        private string nrDeInmatriculare;
+        public int capacitate;
+
+        public Car(string marca, string nrDeInmatriculare, int capacitate)
+        {
+            this.marca = marca;
+            this.nrDeInmatriculare = nrDeInmatriculare;
+            this.capacitate = capacitate;
+        }
+
+        public void Tipareste()
+        {
+            Console.WriteLine($"{marca} {nrDeInmatriculare} {capacitate} cm ");
+        }
+    }
+}
---
+++ Lab6ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Lab6ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*• Ex 2
+Scrieti un program care va modela un autoturism. Un autoturism va avea o
+marca (string), un numar de inmatriculare (string), precum si o
+capacitate cilindrica(int).
+Autoturismul va avea:
+ Campurile corespunzatoare caracteristicilor autoturismului.
+ Un constructor care va initializa campurile acestuia
+ O metoda “Tiparire” care va afisa pe consola descrierea acestuia sub forma : “marca,
+numarDeInmatriculare, capacitateCilindrica”.
+Folositi: clase, campuri, metode, modificatorii de acces adecvati, comentarii
+xml pentru metodele publice.
+Creeati doua obiecte de tipul autoturism in functia “Main” si apelati-le
+metodele “Tipareste”*/
+
+
+            Car auto1 = new Car("Audi", "Ab67dar", 1800);
+            Console.WriteLine("Detalii auto1:");
+            auto1.Tipareste();
+
+            Car auto2 = new Car("Mercedes", "ab56rfd", 7000);
+            Console.WriteLine("Detalii auto2");
+            auto2.Tipareste();
+        }
+    }
+}
